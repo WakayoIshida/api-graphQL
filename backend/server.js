35e9b44e5e19ecce9f123e0134a47c83d974f1bc
WakayoIshida <132349904+WakayoIshida@users.noreply.github.com>
2024-05-19 15:28:31 +0900

    本のデータを更新しました

--- backend/server.js
+++ backend/server.js
@@ -2,14 +2,67 @@ const { ApolloServer, gql } = require('apollo-server');
 
 
 const books = [ // 本の情報を配列で定義
+
     {
-        title: 'Harry Potter and the Chamber of Secrets',
-        author: 'J.K. Rowling',
+        id: 1,
+        title: '1Q84',
+        author: '村上春樹',
+        publishedYear: 2009
     },
     {
-        title: '走れメロス',
-        author: '太宰治',
+        id: 2,
+        title: 'アウト',
+        author: '桐野夏生',
+        publishedYear: 1997
     },
+    {
+        id: 3,
+        title: '告白',
+        author: '湊かなえ',
+        publishedYear: 2008
+    },
+    {
+        id: 4,
+        title: '羊と鋼の森',
+        author: '宮下奈都',
+        publishedYear: 2015
+    },
+    {
+        id: 5,
+        title: '下町ロケット',
+        author: '池井戸潤',
+        publishedYear: 2010
+    },
+    {
+        id: 6,
+        title: '蹴りたい背中',
+        author: '綿矢りさ',
+        publishedYear: 2003
+    },
+    {
+        id: 7,
+        title: 'ブルーアワーにぶっ飛ばす',
+        author: '山内マリコ',
+        publishedYear: 2018
+    },
+    {
+        id: 8,
+        title: '天地明察',
+        author: '冲方丁',
+        publishedYear: 2009
+    },
+    {
+        id: 9,
+        title: '君の膵臓をたべたい',
+        author: '住野よる',
+        publishedYear: 2015
+    },
+    {
+        id: 10,
+        title: 'コンビニ人間',
+        author: '村田沙耶香',
+        publishedYear: 2016
+    }
 ];
 
 
